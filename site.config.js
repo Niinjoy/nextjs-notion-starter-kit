ecb1d120c727d0b8238266ee591c3cf6ad539c49
Niinjoy <39721307+Niinjoy@users.noreply.github.com>
2021-10-13 20:27:19 +0800

    change to official what's new page

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '2498dfb650fd4e8397cb5df0d3ddfdf7',
+  rootNotionPageId: '157765353f2c4705bd45474e5ba8b46c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
